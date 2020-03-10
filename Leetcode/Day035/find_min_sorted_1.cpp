449795f9305d08955cebc73054926d7172f6b4e7
Sujal <sujalahrodia@gmail.com>
2020-03-09 21:03:53 -0400

    Create find_min_sorted_1.cpp

---
+++ Leetcode/Day035/find_min_sorted_1.cpp
@@ -0,0 +1,29 @@
+// without duplicates
+
+class Solution {
+public:
+    int findMin(vector<int>& nums) 
+    {
+        if(nums.size() == 1)
+            return nums[0];
+        
+        int start=0, end=nums.size()-1;
+        
+        //no rotation
+        if(nums[start] < nums[end])
+            return nums[start];
+        
+        while(start<end)
+        {
+            int mid = start+(end-start)/2;
+            
+            if(nums[mid] > nums[end])
+                start=mid+1;
+            else if(nums[mid] < nums[end])
+                end=mid;
+            else
+                end--;
+        }
+        return nums[start];
+    }
+};
